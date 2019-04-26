e1d5d93f74868b8acd1eee62ba2bc0819edc7df2
Emily Wang <Emily36107@Outlook.com>
2019-04-26 08:56:05 +0800

    Releasing v0.0.6.

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as fs from 'fs';
-import * as os from 'os';
 import * as vscode from 'vscode';
 
 import PackReader from './file/PackReader';
@@ -12,7 +11,7 @@ import CopyTask from './model/CopyTask';
 const packReader = new PackReader();
 
 const prepare = (): Promise<undefined> => {
-	return new Promise((resolve, reject) => {
+	return new Promise((resolve) => {
 		const root = vscode.workspace.rootPath;
 		if (root !== undefined && root !== null) {
 			packReader.scan(root, PathConstants.PLACEHOLDER, [PathConstants.NODE_MODULES, PathConstants.GIT]).then(placeholders => {
@@ -26,12 +25,12 @@ const prepare = (): Promise<undefined> => {
 	});
 };
 
-const buildFunction = () => {
+const buildFunction = (projects: Array<string>, tasks: Array<CopyTask>) => {
 	const configuration = vscode.workspace.getConfiguration('node-workspace-builder');
 	new Promise((resolve, reject) => {
 		for (let i = 0; i < vscode.window.terminals.length; i++) {
 			const terminal = vscode.window.terminals[i];
-			if (terminal.name === TerminalHelper.TERMINAL_NAME || terminal.name === TerminalHelper.TERMINAL_NAME_NODE) {
+			if (terminal.name === TerminalHelper.TERMINAL_NAME) {
 				return reject(new Error('You currently have build task or node task running. Please wait until all tasks are finished.'));
 			}
 		}
@@ -39,7 +38,7 @@ const buildFunction = () => {
 	}).then(() => {
 		const terminal = TerminalHelper.createTerminal();
 		terminal.show();
-		packReader.projects.forEach(projectPath => {
+		projects.forEach(projectPath => {
 			let needInstall = false;
 			try {
 				needInstall = !fs.statSync(`${projectPath}${PathConstants.NODE_MODULES}`).isDirectory();
@@ -51,7 +50,7 @@ const buildFunction = () => {
 				TerminalHelper.execNpmInstall(terminal);
 			}
 		});
-		packReader.tasks.forEach((task: CopyTask) => {
+		tasks.forEach((task: CopyTask) => {
 			let needInstallDep = false;
 			if (!configuration.get('buildModulesWithoutInstall')) {
 				try {
@@ -83,6 +82,7 @@ const buildFunction = () => {
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Node Dependencies Helper synchronized dependencies for current workspace!');
 	}).catch(err => {
+		console.error(err);
 		vscode.window.showErrorMessage(err.message);
 	});
 };
@@ -94,34 +94,45 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "node-deps-helper" is now active!');
+	console.log('Congratulations, your extension "node-workspace-builder" is now active!');
 
 	let build = vscode.commands.registerCommand('extension.buildWorkspace', buildFunction);
 	context.subscriptions.push(build);
 
 	vscode.workspace.onDidSaveTextDocument((e: vscode.TextDocument) => {
 		const configuration = vscode.workspace.getConfiguration('node-workspace-builder');
+		const tasks = new Array<CopyTask>();
 		if (configuration.get('autoBuildOnSave')) {
 			let needRebuild = false;
 			let needReprepare = false;
 			const changedFilePath = e.fileName;
 			for (let i = 0; i < packReader.watchPaths.length; i++) {
 				let path = packReader.watchPaths[i];
-				if (changedFilePath.indexOf(path) >= 0) {
+				needReprepare = changedFilePath.indexOf(PathConstants.PACK_JSON) >= 0;
+				if (needReprepare) {
 					needRebuild = true;
-					needReprepare = path === PathConstants.PACK_JSON;
 					break;
+				} else {
+					needRebuild = changedFilePath.indexOf(path) >= 0;
+					if (needRebuild) {
+						tasks.splice(0, tasks.length, ...packReader.tasks.filter(f => changedFilePath.indexOf(f.modulePath) >= 0));
+						break;
+					}
 				}
 			}
 			if (needRebuild) {
-				needReprepare ? prepare().then(buildFunction) : buildFunction();
+				needReprepare ? prepare().then(() => {
+					buildFunction(packReader.projects, packReader.tasks);
+				}) : buildFunction(packReader.projects, tasks);
 			}
 		}
 	});
 	vscode.workspace.onDidChangeWorkspaceFolders(() => {
 		const configuration = vscode.workspace.getConfiguration('node-workspace-builder');
 		if (configuration.get('autoBuildOnFoldersChanged')) {
-			prepare().then(buildFunction);
+			prepare().then(() => {
+				buildFunction(packReader.projects, packReader.tasks);
+			});
 		}
 	});
 
